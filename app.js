45854b45c16571b3de07024fdc6bd14203391055
slashflex <pro.davidsaoud@gmail.com>
2019-11-23 11:06:09 +0100

    API: Handling GET Requests

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours_node.js' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours_node.js' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
 });
 
 const port = 1337;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+    console.log(`App running on port ${port}...`);
 });
