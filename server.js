ab47be7a2dc79af7d7a3d11cebeed6187dc6b58a
slashflex <pro.davidsaoud@gmail.com>
2019-11-30 17:30:38 +0100

    Environment Variables

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = 1337;
+const port = process.env.PORT || 1337;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
