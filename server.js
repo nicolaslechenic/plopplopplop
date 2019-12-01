9a73c457f530e57c44ec00220f62bfb6e9ab3f46
slashflex <pro.davidsaoud@gmail.com>
2019-12-01 15:07:39 +0100

    new mongoose Schema w/ validator

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
@@ -14,7 +15,23 @@ mongoose
     })
     .then(() => console.log('DB connection successfull'));
 
-const app = require('./app');
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
