e6d425e609553f49635145c260d5f812844876c8
slashflex <pro.davidsaoud@gmail.com>
2019-12-02 19:28:23 +0100

    status code changed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -65,7 +65,7 @@ exports.createTour = async (req, res) => {
 
         const newTour = await Tour.create(req.body);
 
-        res.status(200).json({
+        res.status(201).json({
             status: 'success',
             data: {
                 tour: newTour
