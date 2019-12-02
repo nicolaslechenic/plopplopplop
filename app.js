b733a66b12b60c57439fdd157a32280ea275a4da
slashflex <pro.davidsaoud@gmail.com>
2019-12-02 23:04:40 +0100

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ app.use(express.json()); // Middleware
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware 😀');
-    next();
-});
-
-app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
@@ -28,5 +23,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 3. START SERVER
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    });
+});
+
 module.exports = app;
