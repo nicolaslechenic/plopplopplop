fe60617245cf5da5ca459360c73e9124868420a4
slashflex <pro.davidsaoud@gmail.com>
2019-12-02 23:30:42 +0100

    Better Errors and Refactoring

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -24,10 +26,10 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on this server`
-    });
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+// GLOBAL ERROR HANDLING MIDDLEWARE
+app.use(globalErrorHandler);
 
 module.exports = app;
