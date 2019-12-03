c0b170011be931f748cb0b92b0a9161f1595ed8a
slashflex <pro.davidsaoud@gmail.com>
2019-12-03 23:51:22 +0100

    Protecting Tour Routes algorythm

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -18,6 +18,11 @@ const handleValidationErrorDB = err => {
     return new AppError(message, 400);
 };
 
+const handleJWTError = () => new AppError('Invalid token. Please login again', 401);
+
+const handleJWTExpiredError = () =>
+    new AppError('Your token has expired. Please login again', 401);
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -60,6 +65,9 @@ module.exports = (err, req, res, next) => {
         if (error.name === 'CastError') error = handleCastErrorDBB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
         if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+        if (error.name === 'JsonWebTokenError') error = handleJWTError();
+        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+        
         sendErrorProd(error, res);
     }
 };
