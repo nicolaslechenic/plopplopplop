f1c8845143285e85e0a4df209bfda65b8d1dfd4d
slashflex <pro.davidsaoud@gmail.com>
2019-12-04 14:15:04 +0100

    Deleting the current user (not from DB) /w state

--- models/userModel.js
+++ models/userModel.js
@@ -42,7 +42,12 @@ const userSchema = new mongoose.Schema({
     },
     passwordChangedAt: Date,
     passwordResetToken: String,
-    passwordResetExpires: Date
+    passwordResetExpires: Date,
+    active: {
+        type: Boolean,
+        default: true,
+        select: false
+    }
 });
 
 userSchema.pre('save', async function(next) {
@@ -54,6 +59,12 @@ userSchema.pre('save', async function(next) {
 
     // Delete passwordConfirm field
     this.passwordConfirm = undefined;
+    next();
+});
+
+userSchema.pre(/^find/, function(next) {
+    // this points to the current query
+    this.find({ active: { $ne: false } });
     next();
 });
 
