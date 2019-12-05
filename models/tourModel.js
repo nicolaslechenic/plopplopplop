3be181bf2b931b6fedcc990c21a9e392abdd604d
slashflex <pro.davidsaoud@gmail.com>
2019-12-05 16:40:38 +0100

    Improving Read performance with indexes

--- models/tourModel.js
+++ models/tourModel.js
@@ -116,6 +116,9 @@ const tourSchema = new mongoose.Schema(
     }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 tourSchema.virtual('durationWeeks').get(function() {
     return this.duration / 7;
 });
@@ -132,23 +135,6 @@ tourSchema.pre('save', function(next) {
     this.slug = slugify(this.name, { lower: true });
     next();
 });
-
-// Embedding
-// tourSchema.pre('save', async function(next) {
-//     const guidesPromises = this.guides.map(async id => await User.findById(id));
-//     this.guides = await Promise.all(guidesPromises);
-//     next();
-// });
-
-// tourSchema.pre('save', function(next) {
-//     console.log('Will save document...');
-//     next();
-// });
-
-// tourSchema.post('save', function(doc, next) {
-//     console.log(doc);
-//     next();
-// });
 
 // QUERY MIDDLEWARE
 tourSchema.pre(/^find/, function(next) {
