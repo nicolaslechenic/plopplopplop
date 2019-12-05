8c60189102292178445eb3fdc00cb179149c9c78
slashflex <pro.davidsaoud@gmail.com>
2019-12-05 12:31:14 +0100

    Nested routes with express (mergeParam)

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+// POST /tour/5151235/reviews = nested route
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
     .route('/top-5-cheap')
@@ -27,15 +31,6 @@ router
         authController.protect,
         authController.restrictTo('admin', 'lead-guide'),
         tourController.deleteTour
-    );
-
-// POST /tour/5151235/reviews = nested route
-router
-    .route('/:tourId/reviews')
-    .post(
-        authController.protect,
-        authController.restrictTo('user'),
-        reviewController.createReview
     );
 
 module.exports = router;
