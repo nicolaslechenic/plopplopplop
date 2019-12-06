2d6fc4ff0e0df4243cbab409533e00382c2f3e03
slashflex <pro.davidsaoud@gmail.com>
2019-12-07 00:02:34 +0100

    refacored for parcel

---
+++ public/js/alerts.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+export const hideAlert = () => {
+    const el = document.querySelector('.alert');
+    if (el) el.parentElement.removeChild(el);
+};
+
+// Type is either 'success' or 'error'
+export const showAlert = (type, message) => {
+    hideAlert();
+
+    const markUp = `<div class="alert alert--${type}">${message}</div>`;
+    document.querySelector('body').insertAdjacentHTML('afterbegin', markUp);
+
+    window.setTimeout(hideAlert, 5000);
+};
