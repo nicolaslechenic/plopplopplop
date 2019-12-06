55f04607c04e2a621efe5937ddb3b69f0a856967
slashflex <pro.davidsaoud@gmail.com>
2019-12-06 23:18:33 +0100

    logging in users with the API + axios cdn + cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,6 +74,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     let token;
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
     }
 
     if (!token) {
@@ -105,6 +107,34 @@ exports.protect = catchAsync(async (req, res, next) => {
     next();
 });
 
+// Only for rendered pages, no errors
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+    if (req.cookies.jwt) {
+        // 1. Verify the token
+        const decoded = await promisify(jwt.verify)(
+            req.cookies.jwt,
+            process.env.JWT_SECRET
+        );
+
+        // 2. Check if user still exists
+        const currentUser = await User.findById(decoded.id);
+        if (!currentUser) {
+            return next();
+        }
+
+        // 3. Check if user changed password after the token was issued
+        if (currentUser.changedPasswordAfter(decoded.iat)) {
+            return next();
+        }
+
+        // THERE IS A LOGGED IN USER
+        res.locals.user = currentUser;
+        return next();
+    }
+    next();
+});
+
+// Restict access by role
 exports.restrictTo = (...roles) => {
     return (req, res, next) => {
         if (!roles.includes(req.user.role)) {
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
     // 1. Get tours data from collection
     const tours = await Tour.find();
 
@@ -13,7 +13,7 @@ exports.getOverview = catchAsync(async (req, res) => {
     });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
     // 1. Get data for the requested tour (including reviews and guides)
     const tour = await Tour.findOne({ slug: req.params.slug }).populate({
         path: 'reviews',
@@ -27,3 +27,15 @@ exports.getTour = catchAsync(async (req, res) => {
         tour
     });
 });
+
+exports.getLoginForm = (req, res) => {
+    res.status(200).render('login', {
+        title: 'Log into your account'
+    });
+};
+
+exports.getSignupForm = (req, res) => {
+    res.status(200).render('signup', {
+        title: 'Sign up for an account'
+    });
+};
