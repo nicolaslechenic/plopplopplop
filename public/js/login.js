01e9f8a037df81ffd29bfbb3a5541761a0445328
slashflex <pro.davidsaoud@gmail.com>
2019-12-07 00:20:11 +0100

    logging out users

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import axios from 'axios';
-import { showAlert } from './alerts'
+import { showAlert } from './alerts';
 
 export const login = async (email, password) => {
     try {
@@ -22,5 +22,17 @@ export const login = async (email, password) => {
         }
     } catch (err) {
         showAlert('error', err.response.data.message);
+    }
+};
+
+export const logout = async () => {
+    try {
+        const res = await axios({
+            method: 'GET',
+            url: 'http://127.0.0.1:1337/api/v1/users/logout'
+        });
+        if ((res.data.status = 'success')) location.reload(true);
+    } catch (err) {
+        showAlert('error', 'Error logging out! Try again.');
     }
 };
