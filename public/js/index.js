7c49aa903c190be3c2261b98a272035856a1bcf3
slashflex <pro.davidsaoud@gmail.com>
2019-12-18 21:50:41 +0100

    Adding Image Uploads to Form

--- public/js/index.js
+++ public/js/index.js
@@ -10,6 +10,7 @@ const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
+const bookBtn = document.getElementById('book-tour');
 
 // DELEGATION
 if (mapBox) {
@@ -17,27 +18,28 @@ if (mapBox) {
     displayMap(locations);
 }
 
-if (loginForm) {
+if (loginForm)
     loginForm.addEventListener('submit', e => {
         e.preventDefault();
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         login(email, password);
     });
-}
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
 
-if (userDataForm) {
+if (userDataForm)
     userDataForm.addEventListener('submit', e => {
         e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        updateSettings({ name, email }, 'data');
-    });
-}
+        const form = new FormData();
+        form.append('name', document.getElementById('name').value);
+        form.append('email', document.getElementById('email').value);
+        form.append('photo', document.getElementById('photo').files[0]);
 
-if (userPasswordForm) {
+        updateSettings(form, 'data');
+    });
+
+if (userPasswordForm)
     userPasswordForm.addEventListener('submit', async e => {
         e.preventDefault();
         document.querySelector('.btn--save-password').textContent = 'Updating...';
@@ -46,10 +48,16 @@ if (userPasswordForm) {
         const password = document.getElementById('password').value;
         const passwordConfirm = document.getElementById('password-confirm').value;
         await updateSettings({ passwordCurrent, password, passwordConfirm }, 'password');
-    
+
         document.querySelector('.btn--save-password').textContent = 'Save password';
         document.getElementById('password-current').value = '';
         document.getElementById('password').value = '';
         document.getElementById('password-confirm').value = '';
     });
-}
+
+if (bookBtn)
+    bookBtn.addEventListener('click', e => {
+        e.target.textContent = 'Processing...';
+        const { tourId } = e.target.dataset;
+        bookTour(tourId);
+    });
