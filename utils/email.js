66f2c62ba7d1bb908add5231e6bb305916bb6277
slashflex <pro.davidsaoud@gmail.com>
2019-12-27 15:52:57 +0100

    Building a Complex Email Handler

--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,55 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-    // 1. Create a transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.firstName = user.name.split(' ')[0];
+        this.url = url;
+        this.from = `David Saoud <${process.env.EMAIL_FROM}>`;
+    }
+
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // Sendgrid
+            return 1;
         }
-        // Activate in gmail "less secure app" options
-    });
 
-    // 2. Define the email options
-    const mailOptions = {
-        from: 'Slashflex <hello@gmail.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message
-    };
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+            // Activate in gmail "less secure app" options
+        });
+    }
 
-    // 3. Actually send the email
-    await transporter.sendMail(mailOptions);
+    async send(template, subject) {
+        // Send the actual email
+        // 1. Render the html based on a pug template
+        const html = pug.renderFile(`${__dirname}/../views/emails/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        });
+
+        // 2. Define email options
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmlToText.fromString(html)
+        };
+
+        // 3. Create a transport and send email
+        await this.newTransport().sendMail(mailOptions);
+    }
+
+    async sendWelcome() {
+        await this.send('welcome', 'Welcome to the Natours family');
+    }
 };
-
-module.exports = sendEmail;
