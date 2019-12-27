29c5ab5dad546463b7f66f39f654a3a7ac1490a4
slashflex <pro.davidsaoud@gmail.com>
2019-12-27 19:45:29 +0100

    Processing Payments on the Front-End

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,14 +61,14 @@ exports.login = catchAsync(async (req, res, next) => {
         return next(new AppError('Please provide email and password!', 400));
     }
     // 2. Check if user exists and password is correct
-    const user = await User.findOne({ email: email }).select('+password');
+    const user = await User.findOne({ email }).select('+password');
 
     if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password', 401));
     }
 
     // 3. If everything ok, send token to client
-    createSendToken(user, 200, res);
+    createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
