107a02689778bb1c92490f9c649684418d5bb3ef
slashflex <pro.davidsaoud@gmail.com>
2019-12-27 16:26:50 +0100

    Sending Password Reset Emails

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,7 +49,6 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordResetExpires: req.body.passwordResetExpires
     });
     const url = `${req.protocol}://${req.get('host')}/me`;
-    console.log(url);
     await new Email(newUser, url).sendWelcome();
     createSendToken(newUser, 201, res);
 });
@@ -175,18 +174,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
 
     // 3. Send it to user's mail
-    const resetUrl = `${req.protocol}://${req.get(
-        'host'
-    )}/api/v1/users/resetPassword/${resetToken}`;
-
-    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to:${resetUrl}. \nIf you didn't forget your password, please ignore this email!`;
-
     try {
-        // await sendEmail({
-        //     email: user.email,
-        //     subject: 'Your password reset token (valid for 10 mins)',
-        //     message
-        // });
+        const resetUrl = `${req.protocol}://${req.get(
+            'host'
+        )}/api/v1/users/resetPassword/${resetToken}`;
+        await new Email(user, resetUrl).sendPasswordReset();
 
         res.status(200).json({
             status: 'success',
